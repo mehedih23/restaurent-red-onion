1d8a47328272d7f708f3a66b00148d18ec6b0d3d
mehedih23 <nmehedi23@gmail.com>
2022-04-15 11:30:05 +0600

    header with search bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/NavBar/NavBar';
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
+      <Header></Header>
 
     </div>
   );
